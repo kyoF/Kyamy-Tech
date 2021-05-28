9f981136c4b1f30716b3c45b66b8eeca1d7c6f93
Akari Hirai <hirai.akari.la@mynavi.jp>
2021-05-28 17:40:32 +0900

    create signup

--- src/admin/views/signup.py
+++ src/admin/views/signup.py
@@ -10,3 +10,28 @@ signup = Blueprint('signup', __name__)
 def sign_up():
     return render_template('signup/signup.html')
 
+#新規登録処理
+
+@signup.route('/signup', methods=['POST'])
+def create():
+    user = User(
+        undergraduate=request.form.get('underguraduate'),
+        graduate=request.form.get('graduate'),
+        name=request.form.get('name'),
+        user_id=request.form.get('user_id'),
+        password=request.form.get('password'),
+    )
+
+    try:
+        db.session.add(user)    #不備があった際の例外処理
+        db.session.commit()
+    except:
+        flash('入力した情報を再度確認してください', 'error')
+        return redirect(url_for('signup'))
+
+    flash('アカウントが作成されました', 'success')
+    return redirect(url_for('signup'))
+
+    
+
+    return render_template('login.html')    

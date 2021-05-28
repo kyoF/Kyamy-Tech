07a32c2334fc324af956c316b99c5de05b4ca3bd
Yuta Yamagishi <yamagishi.yuta.le@mynavi.jp>
2021-05-28 17:42:26 +0900

    Yutas edit

--- src/admin/views/search.py
+++ src/admin/views/search.py
@@ -1,5 +1,7 @@
 from flask import render_template, request, url_for, session, redirect, flash, Blueprint
 
+from lib.models import Book
+
 from lib.db import db
 
 from .login import login_check
@@ -11,8 +13,20 @@ search = Blueprint('search', __name__)
 def window():
     return render_template('search/window.html')
 
-# @search.route('/result', methods=['POST'])
-@search.route('/result')
+@search.route('/result', methods=['POST'])
 @login_check
 def search_result():
-    return render_template('search/result.html')
+    get_search_result = []
+    search_category = request.form.get('search_category')
+    search_word = request.form.get('search_word')
+
+    if not search_category:
+        categories = Book.query.order_by(Book.category.desc()).filter(Book.title.like(f'%\\{search_word}%'))
+
+    elif not search_word:
+        categories = Book.query.order_by(Book.category.desc()).filter(Book.category==search_category)
+        
+    else:
+        categories = Book.query.order_by(Book.category.desc()).filter(Book.category==search_category).filter(Book.title.like(f'%\\{search_word}%'))
+
+    return render_template('search/result.html', categories=categories)

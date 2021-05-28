9bdfc25d30d472f52dbd266cef32776007ff88c1
kyosuke fujiki <fujiki.kyosuke.tl@mynavi.jp>
2021-05-28 10:48:06 +0900

    update docs

--- src/lib/models/history.py
+++ src/lib/models/history.py
@@ -7,7 +7,7 @@ class History(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
     title = db.relationship('book', backref='history', uselist=False)
-    user_id = db.Column(db.String(64), db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     name = db.relationship('user', backref='history', uselist=False)
     datetime = db.Column(db.Date, nullable=False)
 

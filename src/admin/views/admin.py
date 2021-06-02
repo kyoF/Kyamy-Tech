bcf89d5445afa8e705e80ee42861ce9abce75b5a
Yuta Yamagishi <yamagishi.yuta.le@mynavi.jp>
2021-06-02 10:28:13 +0900

    602yuta1028

--- src/admin/views/admin.py
+++ src/admin/views/admin.py
@@ -8,21 +8,26 @@ admin = Blueprint('admin', __name__)
 
 @admin.route('/')
 def show_admin_window():
-    # sort_category = request.form.get('sort_category')
-    # print(sort_category)
-    # if sort_category=='経済学部人気順':
-    #     admin_book_info = Book.query.order_by(Book.title).all()
-    # elif sort_category=='法学部人気順':
-    #     admin_book_info = Book.query.order_by(Book.id.desc()).all()    
-    # elif sort_category=='理学部人気順':
-    #     admin_book_info = Book.query.order_by(Book.category).all()
-    # elif sort_category=='工学部人気順':
-    #     admin_book_info = Book.query.order_by(Book.category.desc()).all()
-    # elif sort_category=='文学部人気順':
-    #     admin_book_info = Book.query.order_by(Book.name).all()
-    # elif sort_category=='医学部人気順':
-    #     admin_book_info = Book.query.order_by(Book.name()).all()    
-    # else:
-    #     admin_book_info = Book.query.order_by(Book.id).all()
     admin_book_info = Book.query.order_by(Book.id).all()
+    return render_template('admin/admin.html', admin_book_info=admin_book_info)    
+
+@admin.route('/', methods=['POST'])
+def show_admin_window_sort():
+    sort_category = request.form.get('sort_category')
+    # print(sort_category)
+    if sort_category=='経済学部人気順':
+        admin_book_info = Book.query.order_by(Book.keizai.desc()).all()
+    elif sort_category=='法学部人気順':
+        admin_book_info = Book.query.order_by(Book.hougaku.desc()).all()    
+    elif sort_category=='理学部人気順':
+        admin_book_info = Book.query.order_by(Book.rigaku.desc()).all()
+    elif sort_category=='工学部人気順':
+        admin_book_info = Book.query.order_by(Book.kougaku.desc()).all()
+    elif sort_category=='文学部人気順':
+        admin_book_info = Book.query.order_by(Book.bungaku.desc()).all()
+    elif sort_category=='医学部人気順':
+        admin_book_info = Book.query.order_by(Book.igaku.desc()).all()    
+    else:
+        admin_book_info = Book.query.order_by(Book.id).all()
+
     return render_template('admin/admin.html', admin_book_info=admin_book_info)

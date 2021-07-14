ab3a7f4591906cbeb12c16cd031aed908ddfde10
ekusoyk <fujiki.kyosuke@gmail.com>
2021-07-14 21:26:56 +0900

    研修時の完成形

--- src/lib/config.py
+++ src/lib/config.py
@@ -1,7 +1,7 @@
 import os
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{password}@{host}/Z_gen_lib'.format(**{
-    'user' : os.getenv('DB_USER', 'kyosukefujiki'),
-    'password' : os.getenv('DB_PASSWORD', 'mlpmlp'),
+    'user' : os.getenv('DB_USER', 'postgres'),
+    'password' : os.getenv('DB_PASSWORD', 'ZBtH6myf'),
     'host' : os.getenv('DB_HOST', 'localhost:5432'),
 })
 SQLALCHEMY_TRACK_MODIFICATIONS = False
